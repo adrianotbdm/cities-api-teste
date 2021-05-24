40d70701e5b3649dc5494de769a13d72bf42c815
adrianotbdm <adriano_c_@hotmail.com>
2021-05-23 21:14:43 -0300

    Primeiro commit

---
+++ src/main/java/com/github/adrianotbdm/citiesapi/countries/repository/CountryRepository.java
@@ -0,0 +1,6 @@
+package com.github.adrianotbdm.citiesapi.countries.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+}
